4584ef49f9918ae8ffccf0f0295ce54aa5d04f1f
Aras Pranckevicius <aras@unity3d.com>
2011-12-04 13:04:35 +0200

    update from live website

--- common/foot.php
+++ common/foot.php
@@ -3,7 +3,7 @@
 
 <div class="clr"></div>
 <div id="footer">
-2002-2009, <span title='in ascii: Aras Pranckevicius'>Aras Pranckevièius</span> [<a href="mailto:nearaz_at_gmail_dot_com">email</a>]<br><br>
+2002-2011, <span title='in ascii: Aras Pranckevicius'>Aras Pranckevièius</span> [<a href="mailto:nearaz_at_gmail_dot_com">email</a>]<br><br>
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
